41c7fa37e86842b6e59b7f9e971a9adfe1f71e69
Eric Mara <ericmaraoliveira@gmail.com>
2026-06-02 18:50:27 -0300

    remoção de itens não utilizados

--- RestSync_FrontEnd/src/components/Cadastro/CadastroForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CadastroForm() {
-  return (
-    <div>CadastroForm</div>
-  )
-}
-
-export default CadastroForm
--- RestSync_FrontEnd/src/pages/Cadastro/Cadastro.jsx
+++
@@ -1,9 +0,0 @@
-import CadastroForm from '../../components/Cadastro/CadastroForm'
-
-function Cadastro() {
-  return (
-    <div><CadastroForm/></div>
-  )
-}
-
-export default Cadastro
